872b1813d7a062ee7679041a1de4d202ef31351b
Théo Segonds <theo.segonds@inria.fr>
2015-07-16 11:48:42 +0200

    aa

--- ci/update_documentation.sh
+++ ci/update_documentation.sh
@@ -16,5 +16,5 @@ pip install -q bottle zmq zerorpc
 cd ..
 curl -O https://gist.githubusercontent.com/pierre-rouanet/f296ea65a2dbf913ce78/raw/788e4b0433c1dac61aac681562dc59bb333e5e7e/make-doc.py
 # For the tests, to be removed
-eval "sed -e s/poppy-project/$GH_USERNAME/g make-doc.py"
+sed -e s/poppy-project/show0k/g make-doc.py"
 python make-doc.py pypot
