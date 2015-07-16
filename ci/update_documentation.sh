f60b19fd9ebe4164ac0a1953ce0c1696ef67d743
Théo Segonds <theo.segonds@inria.fr>
2015-07-16 11:29:40 +0200

    update doc

--- ci/update_documentation.sh
+++ ci/update_documentation.sh
@@ -15,4 +15,6 @@ pip install -q bottle zmq zerorpc
 # Call @pierre-rouanet python snippet to build the doc
 cd ..
 curl -O https://gist.githubusercontent.com/pierre-rouanet/f296ea65a2dbf913ce78/raw/788e4b0433c1dac61aac681562dc59bb333e5e7e/make-doc.py
+# For the tests, to be removed
+sed -e s/poppy-project/"$GH_USERNAME"/g make-doc.py
 python make-doc.py pypot
