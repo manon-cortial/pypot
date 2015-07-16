c7efbe12fa8ec1bd6c7ed6c8369164d692a961bb
Théo Segonds <theo.segonds@inria.fr>
2015-07-16 12:27:13 +0200

    comment condaenv

--- ci/before_install_linux.sh
+++ ci/before_install_linux.sh
@@ -13,7 +13,7 @@ chmod +x miniconda.sh
 export PATH=/home/travis/miniconda/bin:$PATH
 conda update --yes conda
 conda install --yes -n condaenv pip
-source activate condaenv
+# source activate condaenv
 conda install --yes python=$TRAVIS_PYTHON_VERSION atlas numpy scipy matplotlib
 
 
