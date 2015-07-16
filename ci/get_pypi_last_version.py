505b89d26d7807e88305469d07606fe0622fbb47
Théo Segonds <theo.segonds@inria.fr>
2015-07-16 23:14:32 +0200

    add file

---
+++ ci/get_pypi_last_version.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+import argparse
+import xmlrpclib
+import pip
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description='Get the last Pypi version of a python package')
+    parser.add_argument('package',
+                        type=str,
+                        help='Python package to check version.')
+    args = parser.parse_args()
+
+    pypi = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
+    available = pypi.package_releases(args.package)
+    if not available:
+        print ('0')
+    else:
+        print (available[0])
