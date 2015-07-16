f53b4ceb71cb9a1929ac451899936159a2d8b0ef
Théo Segonds <theo.segonds@inria.fr>
2015-07-16 22:41:41 +0200

    add name

--- ci/update_documentation.sh
+++ ci/update_documentation.sh
@@ -2,6 +2,7 @@
 set -x
 set -e
 
+pypot_src_version=$(python -c "import pypot; print (pypot.__version__)")
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
 git_url=https://$GH_TOKEN@github.com/$GH_USERNAME/$GH_REPO.git
@@ -30,6 +31,18 @@ pushd ..
 
     git clone -b gh-pages $git_url $tmp_repo 
     cp -r $doc_src/_build/html/ $tmp_repo
+
+    if [[ "$TRAVIS" == "true" ]]; then
+        if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+        echo "This is a pull request. No deployment will be done."
+        exit 0
+        fi
+        if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+        echo "Testing on a branch other than master. No deployment will be done."
+        exit 0
+    fi
+
+    # Push the new documentation only if it is not a pull request and we are on master
     pushd $tmp_repo
         git add -A
         git commit -m "doc updates"
