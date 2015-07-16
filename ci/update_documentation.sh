8ce7ffdf0bd2b9b9e6fd32b3deb71c8b2b138c56
Théo Segonds <theo.segonds@inria.fr>
2015-07-16 18:15:38 +0200

    change doc

--- ci/update_documentation.sh
+++ ci/update_documentation.sh
@@ -17,11 +17,11 @@ pip install -q bottle zmq zerorpc
 
 # Build the doc
 pushd ..
-    doc_src="./$GH_REPO/doc"
-    tmp_repo="/tmp/$GH_REPO-doc"
+    doc_src=./"$GH_REPO"/doc
+    tmp_repo=/tmp/"$GH_REPO"-doc
 
-        make -C "$doc_src" clean
-        make -C "$doc_src" html
+        make -C $doc_src clean
+        make -C $doc_src html
 
         if [ -d "$tmp_repo" ] then
             rm -rf "$tmp_repo"/
