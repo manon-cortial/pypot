31001f2bb68ac8d85cbec934be5cbffa0a9f330b
Théo Segonds <theo.segonds@inria.fr>
2015-07-17 16:36:50 +0200

    update vrep tests

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -18,13 +18,14 @@ if [[ "$BUILD" == "test-vrep" ]]; then
     popd
 
     pip install poppy-humanoid
-    python -c "from poppy.creatures import PoppyHumanoid ;
+    echo " Display  poppy.get_object_position('pelvis_visual')"
+    python -c "import sys;
+    from poppy.creatures import PoppyHumanoid ;
     from pypot.vrep import from_vrep ;
-    poppy = PoppyHumanoid(simulator='vrep')"
+    poppy = PoppyHumanoid(simulator='vrep');
+    sys.stdout.write(poppy.get_object_position('pelvis_visual'))"    
     set -e
 fi
-
-
 
 # TODO :)
 which python
