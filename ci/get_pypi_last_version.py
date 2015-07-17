050c936d5650bf00aef1b0ca4c63fe0506ab3452
Théo Segonds <theo.segonds@inria.fr>
2015-07-17 10:57:50 +0200

    change pypi

--- ci/get_pypi_last_version.py
+++ ci/get_pypi_last_version.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python
 import argparse
+try:
+    import xmlrpclib
+except ImportError:
+    import xmlrpc.client as xmlrpclib
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -8,16 +12,11 @@ if __name__ == '__main__':
                         type=str,
                         help='Python package to check version.')
     args = parser.parse_args()
-    try:
-        import xmlrpclib
-        pypi = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
-    except:
-        # For python 3.x
-        import xmlrpc
-        pypi = xmlrpc.client.ServerProxy('http://pypi.python.org/pypi')
-
-    available = pypi.package_releases(args.package)
+    client = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
+    available = client.package_releases(args.package)
     if not available:
         print ('0')
     else:
         print (available[0])
+
+# TODO return packages filename with client.release_urls('package',version)
